71da2b175cedf255217941ab9f87d136cf95bba1
Shovnik Bhattacharya <shovnik.bhattacharya@gmail.com>
2021-04-14 21:43:29 -0400

    Fixed query

--- DataMiningQueries.sql
+++ DataMiningQueries.sql
@@ -1,6 +1,6 @@
----Grouping # of crimes that occur in a particular Borough by the crime cateogory
-SELECT COUNT(SELECT COUNT(generalCrimeID) as numberOfCrimes, GeneralCrime.lsoa, GeneralCrime.minorCategory, LSOA.Borough
+-- Grouping # of crimes that occur in a particular Borough by the crime cateogory
+SELECT COUNT(generalCrimeID) AS numberOfCrimes, lsoa, minorCategory, borough
 FROM GeneralCrime
 INNER JOIN LSOA 
 USING (lsoa)
-GROUP BY GeneralCrime.minorCategory;
+GROUP BY minorCategory, lsoa, borough;
